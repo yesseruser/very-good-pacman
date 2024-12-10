4b0674d209031fecf42b39db592b45b79cb4b6e0
yesseruser <yesseruseryt@gmail.com>
2024-12-10 15:39:28 +0100

    Fix movement

--- src/GameObjects/MovableGameObject.py
+++ src/GameObjects/MovableGameObject.py
@@ -10,12 +10,18 @@ class MovableGameObject(GameObject):
     speed: int
 
     def move(self):
-        if (self.next_direction != Direction.NONE and
+        if (self.next_direction != Direction.NONE and self.next_direction != self.direction and
             self.game.get_tile_at_tuple(
                 self.game.get_tile_from_pixel(
                     self.direction.reversed().get_corner_to_check(self.next_direction,
                                                                   self.pixel_center_pos,
-                                                                  self.game.settings.tile_pixels, (-1, 1)))) != LevelTile.WALL):
+                                                                  self.game.settings.tile_pixels, (-1, 1)))) != LevelTile.WALL and
+            self.game.get_tile_at_tuple(
+                self.game.get_tile_from_pixel(
+                    self.direction.get_corner_to_check(self.next_direction,
+                                                                  self.pixel_center_pos,
+                                                                  self.game.settings.tile_pixels,
+                                                                  (-1, 1)))) != LevelTile.WALL):
             self.direction = self.next_direction
 
         corner1 = self.direction.get_corner_to_check(self.direction.get_relative_direction(),
@@ -31,20 +37,6 @@ class MovableGameObject(GameObject):
 
         if tile1 != LevelTile.WALL and tile2 != LevelTile.WALL:
             self.pixel_center_pos = self.game.get_wrapped_position(self.direction.get_moved_position(self.pixel_center_pos, self.speed))
-
-        """
-        edge_x_right = self.game.get_tile_from_pixel((self.pixel_center_pos[0] + (self.game.settings.tile_pixels / 2) - 1, self.pixel_center_pos[1]))
-        edge_x_left = self.game.get_tile_from_pixel((self.pixel_center_pos[0] - (self.game.settings.tile_pixels / 2), self.pixel_center_pos[1]))
-        edge_y_right = self.game.get_tile_from_pixel((self.pixel_center_pos[0], self.pixel_center_pos[1] + (self.game.settings.tile_pixels / 2)))
-        edge_y_left = self.game.get_tile_from_pixel((self.pixel_center_pos[0], self.pixel_center_pos[1] - (self.game.settings.tile_pixels / 2)))
-
-        if (self.game.get_tile_at(edge_x_right[0], edge_x_right[1])) == LevelTile.WALL:
-            self.direction = Direction.NONE
-            self.pixel_center_pos = (self.game.get_pixel_center_from_tile((edge_x_right[0] - 1, edge_x_right[1]))[0], self.pixel_center_pos[1])
-        if (self.game.get_tile_at(edge_x_left[0], edge_x_left[1])) == LevelTile.WALL:
-            self.direction = Direction.NONE
-            self.pixel_center_pos = (self.game.get_pixel_center_from_tile((edge_x_left[0] + 1, edge_x_left[1]))[0], self.pixel_center_pos[1])
-        """
 
     def update(self):
         self.move()
